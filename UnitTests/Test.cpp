d9e9fffadd5bdc0c44f5b6ec7c9087477f15554b
Jorrit Rouwe <jrouwe@gmail.com>
2021-08-13 15:21:27 +0200

    again

--- UnitTests/Test.cpp
+++ UnitTests/Test.cpp
@@ -9,7 +9,7 @@ TEST_SUITE("Suite")
 #if defined(__x86_64__) || defined(_M_X64)
 		FAIL_CHECK("x64");
 #elif defined(__aarch64__) || defined(_M_ARM64)
-		FAIL_CHECK("arm64")
+		MESSAGE("arm64");
 #endif
 	}
 }
