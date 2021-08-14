4ec34b98991c45c8c110c8c380581134a48566b8
Jorrit Rouwe <jrouwe@gmail.com>
2021-08-14 14:36:23 +0200

    removed test code

--- UnitTests/Test.cpp
+++ UnitTests/Test.cpp
@@ -6,13 +6,6 @@ TEST_SUITE("Suite")
 {
 	TEST_CASE("Test")
 	{
-#if __clang_major__ == 10
-#warning 10
-#elif __clang_major__ == 11
-#warning 11
-#elif __clang_major__ == 12
-#warning 12
-#endif
 		CHECK(true);
 	}
 }
