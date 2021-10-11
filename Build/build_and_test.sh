c5531e25bbf9f82596e888cb0e02483de179c65d
Jorrit Rouwe <jrouwe@gmail.com>
2021-10-11 23:31:56 +0200

    again

--- Build/build_and_test.sh
+++ Build/build_and_test.sh
@@ -1,6 +1,6 @@
-./cmake_linux_clang.sh ReleaseCoverage
+./cmake_linux_clang.sh ReleaseCoverage clang++-12
 cmake --build Linux_ReleaseCoverage
 cmake --build Linux_ReleaseCoverage --target test
 ls -al Linux_ReleaseCoverage/
-llvm-profdata merge -sparse Linux_ReleaseCoverage/default.profraw -o Linux_ReleaseCoverage/default.profdata
-llvm-cov show -format=text Linux_ReleaseCoverage/UnitTests -instr-profile=Linux_ReleaseCoverage/default.profdata > coverage.txt
+llvm-profdata-12 merge -sparse Linux_ReleaseCoverage/default.profraw -o Linux_ReleaseCoverage/default.profdata
+llvm-cov-12 show -format=text Linux_ReleaseCoverage/UnitTests -instr-profile=Linux_ReleaseCoverage/default.profdata > coverage.txt
