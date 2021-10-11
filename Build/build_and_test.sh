598466a818b045e51949791fcb6147afd66c6764
Jorrit Rouwe <jrouwe@gmail.com>
2021-10-11 23:26:14 +0200

    more debugging

--- Build/build_and_test.sh
+++ Build/build_and_test.sh
@@ -1,5 +1,6 @@
 ./cmake_linux_clang.sh ReleaseCoverage
 cmake --build Linux_ReleaseCoverage
 cmake --build Linux_ReleaseCoverage --target test
+ls -al Linux_ReleaseCoverage/
 llvm-profdata merge -sparse Linux_ReleaseCoverage/default.profraw -o Linux_ReleaseCoverage/default.profdata
 llvm-cov show -format=text Linux_ReleaseCoverage/UnitTests -instr-profile=Linux_ReleaseCoverage/default.profdata > coverage.txt
