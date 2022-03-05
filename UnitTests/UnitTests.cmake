f1d41261207f36804acf409d2272d7b1e218ffbd
Jorrit Rouwe <jrouwe@gmail.com>
2022-03-05 20:21:53 +0100

    macos test

--- UnitTests/UnitTests.cmake
+++ UnitTests/UnitTests.cmake
@@ -4,6 +4,7 @@ set(UNIT_TESTS_ROOT ${PHYSICS_REPO_ROOT}/UnitTests)
 # Source files
 set(UNIT_TESTS_SRC_FILES
 	${UNIT_TESTS_ROOT}/Test.cpp
+	${UNIT_TESTS_ROOT}/TickCounter.cpp
 	${UNIT_TESTS_ROOT}/UnitTestFramework.cpp
 	${UNIT_TESTS_ROOT}/UnitTestFramework.h
 	${UNIT_TESTS_ROOT}/UnitTests.cmake
