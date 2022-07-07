a0077f70df93cd5c16a7159bbcbf19bc95687c10
Jorrit Rouwe <jrouwe@gmail.com>
2022-07-07 23:32:46 +0200

    check platform

--- PerformanceTest/PerformanceTest.cpp
+++ PerformanceTest/PerformanceTest.cpp
@@ -26,5 +26,11 @@ int main(int argc, char** argv)
 	return hash != nullptr && (strcmp(hash, "0x485e1d8e739a3c9d") == 0 || strcmp(hash, "0xc29b4c0ea4cf1876") == 0)? 0 : 1;
 	*/
 	cout << "Hello, world!" << endl;
+#if defined(__aarch64__) || defined(_M_ARM64)
+	cout << "Running on ARM" << endl;
+#else
+	cout << "Running on x86" << endl;
+#endif
+	
 	return 0;
 }
