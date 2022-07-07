c0494fcfde47645a2400ad9f72cdcb1cbc530fda
Jorrit Rouwe <jrouwe@gmail.com>
2022-07-07 23:28:06 +0200

    test

--- PerformanceTest/PerformanceTest.cpp
+++ PerformanceTest/PerformanceTest.cpp
@@ -10,6 +10,7 @@ using namespace std;
 // Program entry point
 int main(int argc, char** argv)
 {
+	/*
 	const char *hash = nullptr;
 	for (int argidx = 1; argidx < argc; ++argidx)
 	{
@@ -23,4 +24,7 @@ int main(int argc, char** argv)
 	}
 
 	return hash != nullptr && (strcmp(hash, "0x485e1d8e739a3c9d") == 0 || strcmp(hash, "0xc29b4c0ea4cf1876") == 0)? 0 : 1;
+	*/
+	cout << "Hello, world!" << endl;
+	return 0;
 }
